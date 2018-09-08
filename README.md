dd7dcee8ead562c6a9b8407397a895a13f65ec26
berat <berat.cankar@gmail.com>
2018-09-08 17:22:31 +0300

    Finalizing code

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# say-what-you-see
+# Say What You See Sequence Python Script
+
+Say What You See series where ith element of series is composed of groups in the form as the count of the longest same digit group followed by that digit in (i-1)th element of the sequence.
+
+The first 10 elements of the series:
+* 1
+* 11
+* 21
+* 1211
+* 111221
+* 312211
+* 13112221
+* 1113213211
+* 31131211131221
+* 13211311123113112211
+
+## Prerequisites
+   No prerequisites is needed, but just python3.5 interpreter.
+
+## Usage
+   Simply run sayWhatYouSee.py as the following
+   
+   ```commandline
+   python3 sayWhatYouSee.py
+```
--- sayWhatYouSee.py
+++ sayWhatYouSee.py
@@ -1,12 +1,9 @@
-#####################################
-#        Powered by Yakuza          #
-#####################################
+######################################
+#        Powered by yakuza8          #
+######################################
 #
-# Say What You See series where ith
-# element of series is composed of the
-# immediate previous element's lenght
-# of the contiuous substring of different
-# character followed by that character
+# Say What You See series where ith element of series is composed of the immediate previous element's length
+# of the continuous substring of different character followed by that character
 #
 # The first 10 elements of the series:
 # 1
@@ -20,7 +17,64 @@
 # 31131211131221
 # 13211311123113112211
 
+####### Functions for recursive algorithm #######
+
+def merger(l):
+    '''
+    This function takes a list of strings as a parameter and return list of grouped, consecutive strings
+    :param l: List of strings
+    :return:
+    '''
+    if l == []:
+        print("Error in merge")
+    elif len(l) == 1:
+        return l
+    else:
+        # Divide the list from half
+        halfLength = len(l) / 2
+        left = merger(l[:halfLength])
+        right = merger(l[halfLength:])
+        if left[-1][-1] == right[0][0]:
+            # If left and right have common string from ending and beginning respectively, merge them
+            return left[:-1] + [left[-1] + right[0]] + right[1:]
+        else:
+            # Else return directly
+            return left + right
+
+def seperator(s):
+    '''
+    From any string, return the list composed of each character that are in the string
+    :param s:
+    :return:
+    '''
+    result = []
+    for i in s:
+        result.append(i)
+    return result
+
+
+def formatter(l):
+    '''
+    Takes the list in the form ['aaa', 'b', 'ccccc', ...] and return list of tuples which represents (length of list element, its composer character)
+    In other words, return [(3, 'a'), (1, 'b'), (5, 'c'), ...]
+    :param l:
+    :return:
+    '''
+    result = []
+    for i in l:
+        result.append((len(i), i[0]))
+    return result
+
+#################################################
+
+####### Functions for iterative algorithm #######
+
 def getLongestSubs(n):
+    '''
+    Iterates over the string and parses it in order to create list of tuples from length of the longest contiguous characters and that character
+    :param n:
+    :return:
+    '''
     temp = n
     result = []
     l = len(temp)
@@ -34,6 +88,7 @@ def getLongestSubs(n):
         result.append((times, int(currentElement)))
     return result
 
+#################################################
 
 def assembly(l):
     result = []
@@ -41,8 +96,11 @@ def assembly(l):
         result.append(str(i[0]) + str(i[1]))
     return "".join(result)
 
-
 def sayWhatYouSee(n):
+    '''
+    :param n: The number of elements that you need to have in the output list
+    :return:
+    '''
     series = [1]
     for i in range(1, n):
         # Iterative string processor
@@ -50,47 +108,18 @@ def sayWhatYouSee(n):
 
         # Recursive divide and conquer algorithm
         # series.append(assembly(formatter(merger(seperator(str(series[i-1]))))))
+
     return series
 
 
-####### Functions for recursive algorithm #######
-def merger(l):
-    if l == []:
-        print
-        "Error in merge"
-    elif len(l) == 1:
-        return l
-    else:
-        halfLength = len(l) / 2
-        left = merger(l[:halfLength])
-        right = merger(l[halfLength:])
-        if left[-1][-1] == right[0][0]:
-            return left[:-1] + [left[-1] + right[0]] + right[1:]
-        else:
-            return left + right
+def main():
+    while True:
+        a = int(input("Enter the number of elements : "))
+        if a <= 0:
+            print("Invalid length of sequence")
+            break
+        for i in sayWhatYouSee(a):
+            print(i)
 
-
-def seperator(s):
-    result = []
-    for i in s:
-        result.append(i)
-    return result
-
-
-def formatter(l):
-    result = []
-    for i in l:
-        result.append((len(i), i[0]))
-    return result
-
-
-#################################################
-
-
-while True:
-    a = int(input("Enter the number of elements : "))
-    if a <= 0:
-        print("Invalid number")
-        break
-    for i in sayWhatYouSee(a):
-        print(i)
+if __name__ == "__main__":
+    main()
